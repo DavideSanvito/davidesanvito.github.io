303264c05f7af4b0bdc9e179445a75e58a4a9867
Davide Sanvito <dsanvito90@gmail.com>
2020-12-29 16:48:17 +0100

    New website

---
+++ assets/main.js
@@ -0,0 +1,28 @@
+var sectionHeight = function() {
+  var total    = $(window).height(),
+      $section = $('section').css('height','auto');
+
+  if ($section.outerHeight(true) < total) {
+    var margin = $section.outerHeight(true) - $section.height();
+    $section.height(total - margin - 20);
+  } else {
+    $section.css('height','auto');
+  }
+}
+
+$(window).resize(sectionHeight);
+
+$(document).ready(function(){
+  
+  $("nav ul li").on("click", "a", function(event) {
+    var position = $($(this).attr("href")).offset().top - 160;
+    $("html, body").animate({scrollTop: position}, 400);
+    $("nav ul li a").parent().removeClass("active");
+    $(this).parent().addClass("active");
+    event.preventDefault();    
+  });
+  
+  sectionHeight();
+  
+  $('img').on('load', sectionHeight);
+});
